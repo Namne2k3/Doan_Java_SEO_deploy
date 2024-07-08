53ed3a95dbf00f0e90fd3ff3437ba970f2159825
Namne2k3 <nhpn2003@gmail.com>
2024-07-08 11:53:00 +0700

    add helmet for search page

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./Search.css"
 import TechDisplay from '../../components/TechDisplay/TechDisplay'
 import { useParams } from 'react-router-dom'
+import { Helmet } from 'react-helmet-async'
 const Search = () => {
 
     const params = useParams();
@@ -9,6 +10,11 @@ const Search = () => {
 
     return (
         <div>
+            <Helmet>
+                <title>Kết quả tìm kiếm: {search}</title>
+                <meta name='description' content={search} />
+                <link rel="canonical" href={`https://justtechshop.netlify.app/search`} />
+            </Helmet>
             <TechDisplay title='Kết quả tìm kiếm' search={search} />
         </div>
     )
